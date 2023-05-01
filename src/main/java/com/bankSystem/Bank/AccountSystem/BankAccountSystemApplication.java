9879b5ba80754d1af3180a82c230ebab761b7d68
R4wdha19 <116624370+R4wdha19@users.noreply.github.com>
2023-05-01 11:04:20 +0400

    Created packages

---
+++ src/main/java/com/bankSystem/Bank/AccountSystem/BankAccountSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bankSystem.Bank.AccountSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountSystemApplication.class, args);
+	}
+
+}
