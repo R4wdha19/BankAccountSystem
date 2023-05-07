bbc08701d5b7c000c2f061fa343cc207d9ab3323
R4wdha19 <116624370+R4wdha19@users.noreply.github.com>
2023-05-07 13:53:57 +0400

    CRUD Apis for customer

--- src/main/java/com/bankSystem/Bank/AccountSystem/Controller/CustomerController.java
+++ src/main/java/com/bankSystem/Bank/AccountSystem/Controller/CustomerController.java
@@ -1,6 +1,7 @@
 package com.bankSystem.Bank.AccountSystem.Controller;
 
 import com.bankSystem.Bank.AccountSystem.Model.Customer;
+import com.bankSystem.Bank.AccountSystem.RequestObject.CustomerRequestObject;
 import com.bankSystem.Bank.AccountSystem.Service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -42,14 +44,17 @@ public class CustomerController {
         customerList = customerService.getCustomerByCustomerLastName(lastName);
         return customerList;
     }
+
     @RequestMapping(value = "getCustomerByCustomerName", method = RequestMethod.GET)
     public Customer getCustomerByCustomerName(@RequestParam String firstName, @RequestParam String lastName) {
         return customerService.getCustomerByCustomerName(firstName, lastName);
     }
+
     @RequestMapping(value = "getCustomerByCustomerPhoneNumber", method = RequestMethod.GET)
-    public Customer getCustomerByCustomerPhoneNumber(@RequestParam String phoneNumber){
+    public Customer getCustomerByCustomerPhoneNumber(@RequestParam String phoneNumber) {
         return customerService.getCustomerByCustomerPhoneNumber(phoneNumber);
     }
+
     @RequestMapping(value = "getAllActiveCustomers", method = RequestMethod.GET)
     public List<Customer> getAllActiveCustomers() {
         List<Customer> customerList = new ArrayList<>();
@@ -64,16 +69,69 @@ public class CustomerController {
         return customerList;
     }
 
-            getLatestRow
-    getLatestUpdated
-            getCustomersCreatedAfterDate
-    getCustomersByUpdatedDate
-            getCustomersByCreatedDate
-    deleteCustomerById
-            deleteCustomerByCustomerFirstName
-    deleteCustomerByCustomerLastName
-            deleteCustomerByCustomerName
-    deleteCustomerByCustomerPhoneNumber
-            createCustomer
-    updateCustomer
+    @RequestMapping(value = "getLatestRow", method = RequestMethod.GET)
+    public Customer getLatestRow() {
+        return customerService.getLatestRow();
+    }
+
+    @RequestMapping(value = "getLatestUpdated", method = RequestMethod.GET)
+    public Customer getLatestUpdated() {
+        return customerService.getLatestUpdated();
+    }
+
+    @RequestMapping(value = "getCustomersCreatedAfterDate", method = RequestMethod.GET)
+    public List<Customer> getCustomersCreatedAfterDate(Date createdDate) {
+        List<Customer> customerList = new ArrayList<>();
+        customerList = customerService.getCustomersCreatedAfterDate(createdDate);
+        return customerList;
+    }
+
+    @RequestMapping(value = "getCustomersByUpdatedDate", method = RequestMethod.GET)
+    public List<Customer> getCustomersByUpdatedDate(String updatedDate) {
+        List<Customer> customerList = new ArrayList<>();
+        customerList = customerService.getCustomersByUpdatedDate(updatedDate);
+        return customerList;
+    }
+
+    @RequestMapping(value = "getCustomersByCreatedDate", method = RequestMethod.GET)
+    public List<Customer> getCustomersByCreatedDate(String createdDate) {
+        List<Customer> customerList = new ArrayList<>();
+        customerList = customerService.getCustomersByCreatedDate(createdDate);
+        return customerList;
+    }
+
+    @RequestMapping(value = "deleteCustomerById", method = RequestMethod.POST)
+    public void deleteCustomerById(Integer customerId) {
+        customerService.deleteCustomerById(customerId);
+    }
+
+    @RequestMapping(value = "deleteCustomerByCustomerFirstName", method = RequestMethod.POST)
+    public void deleteCustomerByCustomerFirstName(String firstName) {
+        customerService.deleteCustomerByCustomerFirstName(firstName);
+    }
+
+    @RequestMapping(value = "deleteCustomerByCustomerLastName", method = RequestMethod.POST)
+    public void deleteCustomerByCustomerLastName(String lastName) {
+        customerService.deleteCustomerByCustomerLastName(lastName);
+    }
+
+    @RequestMapping(value = "deleteCustomerByCustomerName", method = RequestMethod.POST)
+    public void deleteCustomerByCustomerName(String firstName, String lastName) {
+        customerService.deleteCustomerByCustomerName(firstName, lastName);
+    }
+
+    @RequestMapping(value = "deleteCustomerByCustomerPhoneNumber", method = RequestMethod.POST)
+    public void deleteCustomerByCustomerPhoneNumber(String phoneNumber) {
+        customerService.deleteCustomerByCustomerPhoneNumber(phoneNumber);
+    }
+
+    @RequestMapping(value = "createCustomer", method = RequestMethod.POST)
+    public void createCustomer(CustomerRequestObject customerRequestObject) {
+        customerService.createCustomer(customerRequestObject);
+    }
+
+    @RequestMapping(value = "updateCustomer", method = RequestMethod.POST)
+    public void updateCustomer(CustomerRequestObject customerRequestObject) {
+        customerService.updateCustomer(customerRequestObject);
+    }
 }
