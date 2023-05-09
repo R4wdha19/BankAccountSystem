1e965b4913193d5152958724a234ab9b081ed00d
R4wdha19 <116624370+R4wdha19@users.noreply.github.com>
2023-05-09 17:18:54 +0400

    Transaction

--- src/main/java/com/bankSystem/Bank/AccountSystem/Service/TransactionService.java
+++ src/main/java/com/bankSystem/Bank/AccountSystem/Service/TransactionService.java
@@ -1,50 +1,101 @@
 package com.bankSystem.Bank.AccountSystem.Service;
 
 import com.bankSystem.Bank.AccountSystem.Model.BankAccountTransaction;
+import com.bankSystem.Bank.AccountSystem.Repository.BankAccountTransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class TransactionService {
-    public BankAccountTransaction getAccountTransactionById();
+    @Autowired
+    BankAccountTransactionRepository bankAccountTransactionRepository;
 
-    public List<BankAccountTransaction> getAllAccountTransactions();
+    public BankAccountTransaction getAccountTransactionById(Integer bankAccountTransactionId) {
+        return bankAccountTransactionRepository.getAccountTransactionById(bankAccountTransactionId);
+    }
 
-    public List<BankAccountTransaction> getAllActiveAccountTransactions();
+    public List<BankAccountTransaction> getAllAccountTransactions() {
+        return bankAccountTransactionRepository.getAllAccountTransactions();
+    }
 
-    public List<BankAccountTransaction> getAllInActiveAccountTransactions();
+    public List<BankAccountTransaction> getAllActiveAccountTransactions() {
+        return bankAccountTransactionRepository.getAllActiveAccountTransactions();
+    }
 
-    public BankAccountTransaction getLatestRow();
+    public List<BankAccountTransaction> getAllInActiveAccountTransactions() {
+        return bankAccountTransactionRepository.getAllInActiveAccountTransactions();
+    }
 
-    public BankAccountTransaction getLatestUpdated();
+    public BankAccountTransaction getLatestRow() {
+        return bankAccountTransactionRepository.getLatestRow();
+    }
 
-    public List<BankAccountTransaction> getAccountTransactionCreatedAfterDate();
+    public BankAccountTransaction getLatestUpdated() {
+        return bankAccountTransactionRepository.getLatestUpdatedRaw();
+    }
 
-    public List<BankAccountTransaction> getAllAccountTransactionByAmount();
+    public List<BankAccountTransaction> getAccountTransactionCreatedAfterDate(Date createdDate) {
+        List<BankAccountTransaction> bankAccountTransactionList = new ArrayList<>();
+        bankAccountTransactionList = bankAccountTransactionRepository.getAccountTransactionCreatedAfterDate(createdDate);
+        return bankAccountTransactionList;
+    }
 
-    public List<BankAccountTransaction> getAccountTransactionsByUpdatedDate();
+    public List<BankAccountTransaction> getAllAccountTransactionByAmount(Double bankAccountTransactionAmount) {
+        List<BankAccountTransaction> bankAccountTransactionList = new ArrayList<>();
+        bankAccountTransactionList = bankAccountTransactionRepository.getAllAccountTransactionByAmount(bankAccountTransactionAmount);
+        return bankAccountTransactionList;
+    }
 
-    public List<BankAccountTransaction> getAccountTransactionsByCreatedDate();
+    public List<BankAccountTransaction> getAccountTransactionsByUpdatedDate(String updatedDate) {
+        List<BankAccountTransaction> bankAccountTransactionList = new ArrayList<>();
+        bankAccountTransactionList = bankAccountTransactionRepository.getAccountTransactionsByUpdatedDate(updatedDate);
+        return bankAccountTransactionList;
+    }
 
-    public List<BankAccountTransaction> getAccountTransactionByCreditCardId();
+    public List<BankAccountTransaction> getAccountTransactionsByCreatedDate(String createdDate) {
+        List<BankAccountTransaction> bankAccountTransactionList = new ArrayList<>();
+        bankAccountTransactionList = bankAccountTransactionRepository.getAccountTransactionsByCreatedDate(createdDate);
+        return bankAccountTransactionList;
+    }
 
-    deleteAccountTransactionById
+    public List<BankAccountTransaction> getAccountTransactionByCreditCardId(Integer creditCardId) {
+        List<BankAccountTransaction> bankAccountTransactionList = new ArrayList<>();
+        bankAccountTransactionList = bankAccountTransactionRepository.getAccountTransactionByCreditCardId(creditCardId);
+        return bankAccountTransactionList;
+    }
 
-
-            deleteAllAccountTransactionsCreatedAfterDate
-
-    deleteAccountTransactionByCreditCardId
-
-            deleteAccountTransactionByCreatedDate
-
-    deleteAccountTransactionByUpdatedDate
-
-            createAccountTransaction
-
-    updateAccountTransaction
-    public void createTransaction(){
+    public void deleteAccountTransactionById() {
 
     }
+
+
+    public void deleteAllAccountTransactionsCreatedAfterDate() {
+
+    }
+
+    public void deleteAccountTransactionByCreditCardId() {
+
+    }
+
+    public void deleteAccountTransactionByCreatedDate() {
+
+    }
+
+    public void deleteAccountTransactionByUpdatedDate() {
+
+    }
+
+    public void createAccountTransaction() {
+
+    }
+
+    public void updateAccountTransaction() {
+
+    }
+
 
 }
